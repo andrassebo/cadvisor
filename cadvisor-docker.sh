b025cc5eba7165da8f3726a6760998cb7a27b604
Andras Sebo <andras.sebo@betssongroup.com>
2018-07-30 10:49:55 +0200

    removed network parameter

--- cadvisor-docker.sh
+++ cadvisor-docker.sh
@@ -4,8 +4,7 @@ sudo docker run -it \
   --volume=/sys:/sys:ro \
   --volume=/var/lib/docker/:/var/lib/docker:ro \
   --volume=/dev/disk/:/dev/disk:ro \
-  --publish=8080:8080 \
-  --network=monitor-network \
+  --publish=8080:8080 \  
   --detach=true \
   --name=cadvisor \
   andrassebo/cadvisor:latest -storage_driver=influxdb -storage_driver_db=cadvisor -storage_driver_host=influxsrv:8086
